baa3fa8d067e6934d680461fe7decb87d5ff703b
novojilovd <novojilovd2@rambler.ru>
2021-05-04 22:52:27 -0400

    Try some input func

---
+++ tryinsert.py
@@ -0,0 +1,4 @@
+name = input("What is your name?\n")
+language = input("What is your favourite language?\n")
+final = f"Hello {name.strip()}, would you like to leane some {language.strip()}"
+print(final)
