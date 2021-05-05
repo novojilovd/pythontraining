0522f17a0997263f23b21fa5c0213d50ffd04022
novojilovd <novojilovd2@rambler.ru>
2021-05-04 23:01:46 -0400

    Playing with lower and upper

--- tryinsert.py
+++ tryinsert.py
@@ -1,4 +1,4 @@
 name = input("What is your name?\n")
 language = input("What is your favourite language?\n")
-final = f"Hello {name.strip()}, would you like to leane some {language.strip()}"
+final = f"Hello {name.strip().lower()}, would you like to learn some {language.strip().upper()}"
 print(final)
