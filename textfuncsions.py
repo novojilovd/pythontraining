8b1a6c35fd5b7f3304a40a5944dc0218b405a67c
novojilovd <novojilovd2@rambler.ru>
2021-05-04 22:42:50 -0400

    Add some methods

---
+++ textfuncsions.py
@@ -0,0 +1,11 @@
+#name = "daniil novojilov"
+#print(name.title())
+#print(name.upper())
+#print(name.lower())
+
+
+first_name = "daniil"
+last_name = "novojilov"
+full_name = f"{first_name} {last_name}"
+message = f"      Hello, I'm {full_name.title()}"
+print(message.lstrip())
