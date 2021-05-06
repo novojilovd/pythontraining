dc5da825a7218bf221a147d79ec1192b711365e6
novojilovd <novojilovd2@rambler.ru>
2021-05-06 22:03:09 +0900

    Training work with tuple

---
+++ tuple.py
@@ -0,0 +1,9 @@
+dishes = ('Meat on the Bone', 'Vagu', 'Cream Brulle', 'Sharlotte')
+for dish in dishes:
+    print(f"There is {dish} in our menu\n")
+
+print('----------------------\n')
+
+dishes = ('Meat on the Bone', 'Vagu', 'Water Salad', 'Ice Creame') 
+for dish in dishes:
+    print(f"There is {dish} in our menu\n")
