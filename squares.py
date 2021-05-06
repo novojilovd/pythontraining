d46716656251fc28fab36961abda97ed66857c52
novojilovd <novojilovd2@rambler.ru>
2021-05-06 14:41:44 +0900

    Training list generating

---
+++ squares.py
@@ -0,0 +1,11 @@
+squares = [value**2 for value in range (1,11)]
+print(squares)
+million = [value for value in range(1,1000001)]
+#print(len(million))
+#for count in range(0,len(million)-1):
+# print(million[count])
+print(min(million))
+print(max(million))
+print(sum(million))
+tri = [value*3 for value in range(3,31)]
+print(tri)
