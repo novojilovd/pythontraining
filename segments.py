ee57e481de3791960a8be3c4cd0d1ac5a029b402
novojilovd <novojilovd2@rambler.ru>
2021-05-06 15:44:29 +0900

    Training with segments, indexes is hell

---
+++ segments.py
@@ -0,0 +1,6 @@
+names = ['Daniil', 'Egor', 'Andrey', 'Denis', 'Arkady']
+for name in names[0:3]:
+ print(f"{name.lower()} is good boy")
+for name in names[3:5]:
+ print(f"{name.lower()} is bad boy")
+
