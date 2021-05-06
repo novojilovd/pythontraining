b53b3f842a1ffd86f311b6b2c926d91fadcbe489
novojilovd <novojilovd2@rambler.ru>
2021-05-06 14:30:38 +0900

    Training with cycles

--- listcount.py
+++ listcount.py
@@ -1,3 +1,16 @@
 animals = ['cat', 'dog', 'bird', 'monkey']
 for animal in animals:
  print(f"{animal.upper()} is cool!")
+
+for count in range(1,6):
+ print(count)
+
+numbers = list(range(1,6))
+print(numbers)
+
+deci = []
+triples = []
+for count in range(1,11):
+    deci.append(count**3)
+    triples.append(3**count)
+print(f"{triples}\n{deci}")
