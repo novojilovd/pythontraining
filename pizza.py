b851bf65864885ff671ff1e94f2610a4864253ac
novojilovd <novojilovd2@rambler.ru>
2021-05-07 16:29:37 +0900

    Make pizza.py better

--- pizza.py
+++ pizza.py
@@ -1,12 +1,13 @@
 toppings = ['cheese', 'mushrooms', 'meat', 'onions', 'pineapple', 'apple', 'fish', 'cacao', 'watermellon', 'candy']
 count = int(input("How much toppings do you want?\n"))
-requested_toppings = []
+order = "You add: "
 for i in range(count):
     topping = input(f"What is your {i+1} topping?\n")
     if topping.lower() in toppings:
-        requested_toppings.append(topping)
-        print(f"Adding extra {topping.lower()}\n") 
+        print(f"Adding extra {topping.lower()}\n")
+        order = order + f"{topping}, "
     else:
         print("We haven't this topping\n")
 
-print("\nFinishing making your pizza!")
+print("\nFinishing making your pizza!\n")
+print(order[:-2],"!!!")
