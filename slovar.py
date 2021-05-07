cdea23daab34fbc23eaf0b818571d807b411f3cf
novojilovd <novojilovd2@rambler.ru>
2021-05-07 23:25:32 +0900

    Start trainint dictionaries

---
+++ slovar.py
@@ -0,0 +1,6 @@
+floppa_0 = {'type':'Fiery', 'color':'White'}
+print(f"You meet {floppa_0['type']} Floppa")
+print(f"It is {floppa_0['color']} Floppa")
+floppa_0['Age'] = 14
+floppa_0['Shield'] = True
+print(floppa_0)
