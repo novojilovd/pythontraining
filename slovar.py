b1753dd831ce607969a88122509725302e399039
novojilovd <novojilovd2@rambler.ru>
2021-05-08 22:16:53 +0900

    Got get. Are you got it?

--- slovar.py
+++ slovar.py
@@ -4,3 +4,9 @@ print(f"It is {floppa_0['color']} Floppa")
 floppa_0['Age'] = 14
 floppa_0['Shield'] = True
 print(floppa_0)
+if floppa_0.get('Attack') == None:
+    print("Floppa dont attac")
+    floppa_0['Attack'] = 10
+    print("But...\n")
+    print("Floppa hit you\n")
+    print(f"\tYou get {floppa_0['Attack']} damage")
