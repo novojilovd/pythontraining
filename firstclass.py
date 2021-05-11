4b4dedc76cec962cb6a993e9b45a061e28fcae9f
novojilovd <novojilovd2@rambler.ru>
2021-05-11 22:17:26 +0900

    Add second object'

--- firstclass.py
+++ firstclass.py
@@ -13,6 +13,12 @@ class Dog():
 my_dog = Dog(6, 'wille', 'Jessi')
 
 print(f"My dog's name is {my_dog.name}.")
-print(f"My dog is {my_dog.age} years old.")
+print(f"My dog is {my_dog.age} years old.\n")
 my_dog.sit()
 my_dog.roll_over()
+
+your_dog = Dog(10, 'my', 'dog')
+print(f"Your dog's name is {your_dog.name}.")
+print(f"Your dog is {your_dog.age} years old.\n")
+your_dog.sit()
+your_dog.roll_over()
