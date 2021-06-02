2b2613d9e8d98e930d873caeba6e266f0d64de80
novojilovd <novojilovd2@rambler.ru>
2021-06-02 00:21:24 -0400

    Little change algoitm, 2,3,4 work, need add some extra if for other

--- spirale.py
+++ spirale.py
@@ -1,34 +1,59 @@
 N = int(input("Write how big matrix: \n"))
 n = 0
 m = 0
+
+if (N % 2): 
+    k = (N // 2) + 1
+    check = False
+else:   
+    k = N // 2
+    check = True
+
 a = [i+1 for i in range(N**2)]
 b = [[0] * N for i in range(N)]
-while m < N:
-    
+
+for j in range(k):
+    print(m,' ',N)
     for i in range(m, N):
         b[m][i] = a[n]
         n += 1
 
-    m += 1
+    for name in b[::]:
+        print(name)
+    print('\n')
 
-    for i in range(m, N):
+    if (j+1) == k and not check: break
+
+    for i in range(m + 1, N):
         b[i][N-1] = a[n]
         n += 1
-    
-    if (m+1) == N:
-        for i in range(m, N):
+
+    for name in b[::]:
+        print(name)
+    print('\n')
+
+    for i in range(m + 1, N):
+        if m > 0 and (N-2) == m:
             b[N-1][N-i] = a[n]
-            n += 1
-    else:
-        for i in range(m, N):
+        else: 
             b[N-1][N-1-i] = a[n]
-            n += 1 
-
-    for i in range(m, N-1):
-        b[N-1-i][m-1] = a[n]
         n += 1
-    
-    N -= 1
 
-for part in b[::]:
-    print(part)
+    for name in b[::]:
+        print(name)
+    print('\n')
+
+    if (j+1) == k and check: break
+    
+    for i in range(m + 1, N - 1):
+        b[N-1-i][m] = a[n]
+        n += 1
+
+    for name in b[::]:
+        print(name)
+    print('\n')
+
+    m += 1
+    N -= 1
+for name in b[::]:
+    print(name)
