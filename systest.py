5cfbd8d174bc885003fd3d2621d18833965a2274
Даниил Новожилов <novojilovd2@rambler.ru>
2022-05-02 20:46:23 +0900

    Try more sys module

--- systest.py
+++ systest.py
@@ -6,6 +6,17 @@ def more(text, numlines=15):
             if text and input('More?') not in ['y', 'Y']: break
     except:
         print('No text')
+
+def grail(x):
+    raise TypeError('one')
+
 if __name__ == '__main__':
-    import sys
-    more(open(sys.argv[0]), 5)
+    import traceback, sys
+    #more(open(sys.argv[0]), 5)
+    try:
+        grail('Daniil')
+    except:
+        exc_info = sys.exc_info()
+        print(exc_info[0])
+        print(exc_info[1])
+        traceback.print_tb(exc_info[2])
