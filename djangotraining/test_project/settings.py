1386fa6aa995292e1296db2f4b8e29ac0191ff1c
novojilovd <novojilovd2@rambler.ru>
2022-06-18 06:23:17 -0400

    Made blog app with DetailView

--- djangotraining/test_project/settings.py
+++ djangotraining/test_project/settings.py
@@ -31,8 +31,8 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
-    'blog.apps.BlogConfig'
-#   'posts.apps.PostsConfig',
+    'blog.apps.BlogConfig',
+    'posts.apps.PostsConfig',
 #   'pages.apps.PagesConfig',
     'django.contrib.admin',
     'django.contrib.auth',
