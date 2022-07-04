14e0f71cd3fcff53c9434dfc0ebe43f587a6dc21
novojilovd <novojilovd2@rambler.ru>
2022-07-03 23:56:47 -0400

    Change settings for allauth package using

--- djangotraining_book_project/book_project/settings.py
+++ djangotraining_book_project/book_project/settings.py
@@ -39,13 +39,26 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-]
+    'django.contrib.sites',
 
-AUTH_USER_MODEL = 'users.CustomUser'
+    'allauth',
+    'allauth.account',
+]
 
 LOGIN_REDIRECT_URL = 'home'
 
-LOGOUT_REDIRECT_URL = 'home'
+ACCOUNT_LOGOUT_REDIRECT = 'home'
+
+SITE_ID = 1
+
+AUTHENTICATION_BACKENDS = (
+    'django.contrib.auth.backends.ModelBackend',
+    'allauth.account.auth_backends.AuthenticationBackend',
+)
+
+EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+
+AUTH_USER_MODEL = 'users.CustomUser'
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
