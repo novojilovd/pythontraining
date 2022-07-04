42d68107e70ec495be5a88474e116f7ecc7d3806
novojilovd <novojilovd2@rambler.ru>
2022-07-04 01:37:19 -0400

    Move secret key to env + debug

--- djangotraining_book_project/book_project/settings.py
+++ djangotraining_book_project/book_project/settings.py
@@ -20,12 +20,12 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'ygy*2h+=m$(s5lq1cw-868@)y51#zpc8g@kprrun2dxuo2z@&2'
+SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = int(os.environ.get('Degub', default=0))
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1',]
 
 
 # Application definition
