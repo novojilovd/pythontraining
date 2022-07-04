d48ad23ea1eae5bddf50c50df443485c7d1da90b
novojilovd <novojilovd2@rambler.ru>
2022-07-04 01:38:59 -0400

    Fix typos

--- djangotraining_book_project/book_project/settings.py
+++ djangotraining_book_project/book_project/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = int(os.environ.get('Degub', default=0))
+DEBUG = int(os.environ.get('DEBUG', default=0))
 
-ALLOWED_HOSTS = ['127.0.0.1',]
+ALLOWED_HOSTS = []
 
 
 # Application definition
