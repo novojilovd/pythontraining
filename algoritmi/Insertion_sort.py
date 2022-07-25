a213633e6e029e60bae6684d59871a5925e9bcc2
novojilovd <novojilovd2@rambler.ru>
2022-07-25 00:25:03 -0400

    Correct name

---
+++ algoritmi/Insertion_sort.py
@@ -0,0 +1,31 @@
+a = [2,1,9,8,7,6,5,4,3,2,1]
+u = 5    
+def sortasc(a):
+
+    for j in range(1, len(a)):
+        key = a[j]
+        i = j - 1
+        while i >= 0 and a[i] > key:
+            a[i + 1] = a[i]
+            i = i - 1
+        a[i + 1] = key
+
+    print(a)
+
+def sortdesc(a):
+
+    for j in range(1, len(a)):
+        key = a[j]
+        i = j - 1
+        while i >= 0 and a[i] < key:
+            a[i + 1] = a[i]
+            i = i - 1
+        a[i + 1] = key
+              
+    print(a)
+
+if __name__ == '__main__':
+    sortasc(a[:])
+    sortdesc(a[:])
+
+    print(a.index(u))
