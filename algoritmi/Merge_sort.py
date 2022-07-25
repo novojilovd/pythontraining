3d1e62ea9623bddbb943030262f59a5c99165676
novojilovd <novojilovd2@rambler.ru>
2022-07-25 04:57:53 -0400

    Add while version, correct for version, change script for choose version

--- algoritmi/Merge_sort.py
+++ algoritmi/Merge_sort.py
@@ -1,21 +1,61 @@
-def merge(A, p, q, r):
-    nL = q - p
+def merge_while(A, p, q, r):
+    nL = q - p + 1
     nR = r - q
-    L = []
-    R = []
+    L = [True for i in range(nL + 1)]
+    R = [True for i in range(nR + 1)]
+    print(nL, nR, ' ', p, q, r)
+    for i in range(0, nL):
+        L[i] = A[p + i]
+    for j in range(0, nR):
+        R[j] = A[q + 1 + j]
 
-    for i in range(nL):
-        L.append(A[p + i])
-    for j in range(nR):
-        R.append(A[q + j])
-
-    L.append(1000000)
-    R.append(1000000)
+    L[nL] = 1000000
+    R[nR] = 1000000
+    print(L, R)
+    print('-------------')
     
     i = 0
     j = 0
+    k = p
+ 
+    while i < nL and j < nR:
+        if L[i] <= R[j]:
+            A[k] = L[i]
+            i += 1
+        else:
+            A[k] = R[j]
+            j += 1
+        k += 1
+ 
+    while i < nL:
+        A[k] = L[i]
+        i += 1
+        k += 1
+ 
+    while j < nR:
+        A[k] = R[j]
+        j += 1
+        k += 1
+    
+def merge_for(A, p, q, r):
+    nL = q - p + 1
+    nR = r - q
+    L = [True for i in range(nL + 1)]
+    R = [True for i in range(nR + 1)]
+    print(nL, nR, ' ', p, q, r)
+    for i in range(0, nL):
+        L[i] = A[p + i]
+    for j in range(0, nR):
+        R[j] = A[q + 1 + j]
 
-    for k in range(p, r):
+    L[nL] = 1000000
+    R[nR] = 1000000
+    print(L, R)
+    print('-------------')
+ 
+    i = 0
+    j = 0
+    for k in range(p, r + 1):
         if L[i] <= R[j]:
             A[k] = L[i]
             i += 1
@@ -23,15 +63,18 @@ def merge(A, p, q, r):
             A[k] = R[j]
             j += 1
 
-def merge_sort(A, p ,r):
+def merge_sort(A, p, r):
     if p < r:
-        q = (p + r) // 2
-        print(q)
+ 
+        q = p + (r-p) // 2
+ 
         merge_sort(A, p, q)
         merge_sort(A, q + 1, r)
-        merge(A, p, q, r)
+        merge_while(A, p, q, r)
+        print('Merged A', A)
 
 if __name__ == '__main__':
-    A = [1,3,4,2,6,8,2,3,4,5,6,8]
-    merge_sort(A, 0, len(A))
+    A = [1,3,4,-2,2,6,8,2,3,4,5,6,8]
+    print(A)
+    merge_sort(A, 0, len(A) - 1)
     print(A)
