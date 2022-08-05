350eb5c5f3b4dab6145fc7ede60cf5f9e79d8fc5
novojilovd <novojilovd2@rambler.ru>
2022-08-05 01:06:54 -0400

    Quick sort, O(n lgn), no extra memory

---
+++ algoritmi/Quicksort.py
@@ -0,0 +1,20 @@
+def quicksort(A, p, r):
+    if p < r:
+        q = partition(A, p, r)
+        quicksort(A, p, q - 1)
+        quicksort(A, q + 1, r)
+
+def partition(A, p, r):
+    x = A[r]
+    i = p - 1
+    for j in range(p, r):
+        if A[j] <= x:
+            i += 1
+            A[i], A[j] = A[j], A[i]
+    A[i + 1], A[r] = A[r], A[i + 1]
+    return i + 1
+
+if __name__ == "__main__":
+    A = [1,2,3,4,5,4,3,2,1]
+    quicksort(A, 0, len(A) - 1)
+    print(A)
