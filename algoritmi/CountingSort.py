be9cbe32e09e7368913db0e62e333985cab126a9
novojilovd <novojilovd2@rambler.ru>
2022-08-30 08:04:01 +0900

    Some little corrections

--- algoritmi/CountingSort.py
+++ algoritmi/CountingSort.py
@@ -1,4 +1,5 @@
-def counting_sort(A, B, k):
+def counting_sort(A, k):
+    B = [False for i in range(len(A))]
     C = [False for i in range(k + 1)]
     
     for j in range(len(A)):
@@ -10,9 +11,9 @@ def counting_sort(A, B, k):
     for j in range(len(A) - 1, -1, -1):
         B[C[A[j]] - 1] = A[j]
         C[A[j]] = C[A[j]] - 1
+    return B
+
 
 if __name__ == "__main__":
-    A = [5,5,5,4,3,2,1,0]
-    B = [False for i in range(len(A))]
-    counting_sort(A, B, max(A))
-    print(B)    
+    A = [55, 54, 52, 14, 3, 2, 1, 0]
+    print(counting_sort(A, max(A)))
