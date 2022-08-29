e9ba8fe4cc093b0e6e416c21ba514f1715af2c40
novojilovd <novojilovd2@rambler.ru>
2022-08-30 08:11:42 +0900

    'ls -t' parody

---
+++ bigfiles.py
@@ -0,0 +1,12 @@
+import os, sys, glob
+dirname = os.getcwd() if len(sys.argv) == 1 else sys.argv[1]
+
+allsizes = []
+allpy = glob.glob(dirname + os.sep + '*.py')
+for filename in allpy:
+    filesize = os.path.getsize(filename)
+    allsizes.append((filesize, filename))
+
+allsizes.sort()
+print(allsizes[:2])
+print(allsizes[-2:])
